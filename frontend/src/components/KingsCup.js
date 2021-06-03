85a31f89305c9af32d8ad1b12c110920dd8277df
Nathan Fedzen <nathanfedzen@gmail.com>
2021-06-03 17:54:05 -0600

    switched LoginPage.js to functional component

--- frontend/src/components/KingsCup.js
+++ frontend/src/components/KingsCup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import socketIOClient from 'socket.io-client'
 import CardCollection from './CardCollection'
 import BeerCan from './BeerCan'
@@ -13,7 +13,133 @@ const ENDPOINT = 'http://localhost:3003/'
 const socket = socketIOClient(ENDPOINT)
 //this.props.roomCode passed down from KingsGameLobby.js
 
+// export default function KingsCup ({history, name, roomCode}) {
+  // const [gameStatus, setGameStatus] = useState(false)
+  // const [action, setAction] = useState('Choose a Card!')
+  // const [clicks, setClicks] = useState(0)
+  // const [turn, setTurn] = useState(0)
+  // const [canPopped, setCanPopped] = useState('false')
+  // const [canStatus, setCanStatus] = useState(popped)
+  // const [players, setPlayers] = useState([])
+  // const [deck, setDeck] = useState([])
+  // const [loser, setLoser] = useState('')
+  // const nameRoomObject = {name: name, room: roomCode}
+  
+//   useEffect(() => {
+   
+//     const nameRoomObject = {name: name, room: room}
+//     console.log("component did mount hit")
+//     //room will change to this.props.roomCode
+//     socket.emit('room', room)
+//     socket.emit('new-player', nameRoomObject)
+
+//     socket.on('game-started', message => {
+//       setGameStatus(true)
+//     })
+
+//     socket.on('populate-deck', newDeck => {
+//       setDeck({deck: newDeck})
+//     })
+
+//     socket.on('add-player', players => {
+//       if(players){
+//         setPlayers({players: players})
+//       }
+//     })
+
+//     socket.on('update-players', players => {
+//       setPlayers({players: players})
+//     })
+
+//     socket.on('next-player', object => {
+//       nextPlayersTurn(object)
+//     })
+
+
+//     socket.on('game-over', message => {
+//       findAction()
+//     })
+
+//     socket.on('can-pop', name => {
+//       setCanPopped(true)
+//       setCanStatus(popped)
+//       setLoser(name)
+//       setAction({action: `Can Popped! ${name} finish your drink and start a new game!`})
+//     })
+//   })
+
+//   findAction = (cardValue, player) => {
+//     const object = {
+//       player: player,
+//       clicks: clicks,
+//       roomCode: roomCode
+//     }
+//     if(!canPopped){
+//       switch(cardValue) {
+//         case 'ACE':
+//           setAction('Waterfall')
+//           break;
+//         case "2":
+//           setAction('Pick someone to drink')
+//           break;
+//         case "3":
+//           setAction('Take a drink')
+//           break;
+//         case "4":
+//           setAction('Floor')
+//           break;
+//         case "5":
+//           setAction('Guys')
+//           break;
+//         case "6":
+//           setAction('Chicks')
+//           break;
+//         case "7":
+//           setAction('Heaven')
+//           break;
+//         case "8":
+//           setAction('Date')
+//           break;
+//         case "9":
+//           setAction('Rhyme')
+//           break;
+//         case "10":
+//           setAction('Categories')
+//           break;
+//         case 'JACK':
+//           setAction('Never Have I Ever')
+//           break;
+//         case 'QUEEN':
+//           setAction('Question Master')
+//           break;
+//         case 'KING':
+//           setAction('Make a Rule')
+//           break;
+//         default:
+//           break;
+//         }
+//       object.clicks = object.clicks + 1
+//       this.setState({clicks: this.state.clicks + 1})
+//       socket.emit('pop-can', object)
+//     } 
+    
+//   }
+    
+//   startGame = () => {
+//     socket.emit('start-game', this.props.roomCode)
+//   }
+  
+//   nextPlayersTurn = (object) => {
+//     socket.emit('next-players-turn', object)
+//   }
+
+
+// }
+
+
 class KingsCup extends Component {
+
+  
   
   state = {
     gameStatus: false,
@@ -27,6 +153,7 @@ class KingsCup extends Component {
     loser: ""
   }
   
+ 
   componentDidMount(){
     const name = this.props.name
     const room = this.props.roomCode
@@ -68,7 +195,6 @@ class KingsCup extends Component {
       this.setState({canStatus: popped})
       this.setState({loser: name})
       this.setState({action: `Can Popped! ${name} finish your drink and start a new game!`}) 
-      
     })
   }
 
--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
+
+// class LoginPage extends Component {
+  
+  
+  
+  //   render(){
+    //     return (
+      //       <section>
+      //         <div className='action-bar'>
+      //         </div>
+      //         <div className='form'>
+      //             <h1>Welcome to The Pregame</h1>
+      //           <PlayerForm history={this.props.history} addPlayer={this.props.addPlayer} />
+      //         </div>
+      //       </section>
+      //     );
+      //   }
+      // }
+      
+      // export default LoginPage;
+
+
+import React, {useState} from 'react'
+      
 import PlayerForm from './PlayerForm'
 
-class LoginPage extends Component {
+export default function LoginPage({history, addPlayer}) {
 
-
-
-  render(){
-    return (
-      <section>
+  return(
+    <section>
         <div className='action-bar'>
         </div>
         <div className='form'>
             <h1>Welcome to The Pregame</h1>
-          <PlayerForm history={this.props.history} addPlayer={this.props.addPlayer} />
+          <PlayerForm history={history} addPlayer={addPlayer} />
         </div>
       </section>
-    );
-  }
+  )
 }
-
-export default LoginPage;
